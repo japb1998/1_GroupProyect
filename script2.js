6ffec0f72453919a55ff43a9421ba99b820bf6b5
Kelly Smith <kdalton77@hotmail.com>
2020-10-26 23:50:13 -0400

    completed getrecipe function

--- script2.js
+++ script2.js
@@ -1,18 +1,28 @@
 
-var $apiKey="399885ef1dffc6fdfd42c5f1a6bdb34c";
-var $appId="b2baeb67";
-var $cuisineBtn=(".")
-var $cuisine=["Chinese", "Mexican", "Italian", "American", "Mediterranean", "Indian"];
+var apiKey="399885ef1dffc6fdfd42c5f1a6bdb34c";
+var appId="b2baeb67";
+var cuisineBtn=(".")
+var cuisine=["Pasta", "Quick", "Soup", "Pizza", "Vegetable", "Sandwich"];
 
-function getRecipes(cuisine) {
-    console.log(cuisine);
-    var recipeURL="https://api.edamam.com/search?&app_id=" + appId + "app_key=" + apiKey + "&cuisineType=" + cuisine;
+function getRecipes() {
+    console.log("hungry");
+    var cuisine=$(this).attr("id");
+    var recipeURL="https://api.edamam.com/search?q=" + cuisine + "&app_id=" + appId + "&app_key=" + apiKey;
     $.ajax ({
         url: recipeURL,
         method: "GET"
-    })
-    .then(function(response) {
+    }).then(function(response){
         console.log(response);
+        response.hits.forEach(element => {
+            console.log(element);
+            $(".name").append(element.recipe.label);
+            $(".recipe").append(element.recipe.url);
+            
+        });
     })
-}
-getRecipes();
+
+    }
+
+    $(document).on("click", "button", getRecipes)
+    
+
