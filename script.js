a81a1dfe83d5dd7387a9b21c55e670ce254bf2be
Javier <javierperez@javiers-mbp.home>
2020-10-28 11:21:03 -0400

    styling and javascript suggestions

--- script.js
+++ script.js
@@ -5,28 +5,26 @@ moviesArray = [];
 //function to take the values of the genres we want to display
 function checkBox(name) {
     values = [];
-    let checkBoxes = document.querySelectorAll(`input[name=${name}]:checked`);
-    checkBoxes.forEach(element => {
-        values.push($(element).val());
+    let checkBoxes = $(`input[name=${name}]:checked`);
+    console.log(checkBoxes[0]);
 
-    });
+    for (var i = 0; i < checkBoxes.length; i++) {
+        console.log(checkBoxes[i])
+        values.push(checkBoxes[i].value)
+    }
     return values;
 }
 //looping through the movies array created by generate
 function displayMovies(movieArr) {
-    $('.moviesDisplay').empty();
-    movieArr.forEach((movie) => {
-        ;
-        var movieDiv = $('<div>');
-        movieDiv.addClass('movie');
-        movieDiv.text(movie.title);
-        $('.moviesDisplay').append(movieDiv);
+    $('#poster').empty();
+    movieArr.forEach((movie) => { 
+        displayInfo(movie.title);
     })
 }
 //click event to generate the movie with the new parameters
 $('#generate').on('click', function () {
     console.log('click');
-    moviesArray= []; 
+    moviesArray = [];
     $('.moviesDisplay').empty();
     checkBox('genre');
     if ($('.yearInput'))(
@@ -41,10 +39,10 @@ $('#generate').on('click', function () {
         method: 'GET'
     }).then((res) => {
         console.log(res)
-        for(var i= 0; i < 5 ; i++){
-       var element = res.results[i];
+        for (var i = 0; i < 5; i++) {
+            var element = res.results[i];
             moviesArray.push(element);
-            
+
         };
         // displaying the movies list
         displayMovies(moviesArray);
@@ -69,27 +67,38 @@ function releasedYear(option) {
         $('.year-display').empty();
     }
 };
-function displayInfo(movie){
- var displayedInfo = $(movie).text();
-var settings= {
-    url:`http://www.omdbapi.com/?t=${displayedInfo}&apikey=9aaffc86`,
-    method:'GET'
-}
- $.ajax(settings).then((res)=>{console.log(res)
-var movieTitle = $('<h1>');
-var moviePoster = $('<img>');
-var movieDescription=$('<p>');
-movieTitle.text(res.Title);
-moviePoster.attr('src',res.Poster);
-movieDescription.text(res.Plot);
-$('#poster').append(movieTitle).append(moviePoster).append(movieDescription);
-})
+
+function displayInfo(movie) {
+    var settings = {
+        url: `http://www.omdbapi.com/?t=${movie}&apikey=9aaffc86`,
+        method: 'GET'
+    }
+    $.ajax(settings).then((res) => {
+        if(res.Response != "False"){
+        console.log(res)
+        var cardDiv = $('<div>');
+        // cardDiv.addClass('row');
+        var ColDiv =$('<div>').addClass('post col s12 m6');
+        var cardContent = $('<div>').addClass('card-content white-text');
+        var cardColor= $('<div>').addClass('card blue-grey darken-4');
+        var cardTitle = $('<span>').addClass('card-title');
+        var moviePoster = $('<img>');
+        var movieDescription = $('<p>');
+        moviePoster.attr('src', res.Poster);
+        movieDescription.text(res.Plot);
+        cardTitle.text(res.Title);
+        cardContent.append(cardTitle).append(moviePoster).append(movieDescription);
+        cardColor.append(cardContent);
+        ColDiv.append(cardColor);
+        cardDiv.append(ColDiv);
+        $('#poster').append(cardDiv);}
+    })
 }
 
 $(document).on('change', 'input[name="radio"]', function (e) {
     releasedYear($(e.target))
 })
-$(document).on('click','.movie',function(e){
+$(document).on('click', '.movie', function (e) {
     $('#poster').empty();
     displayInfo(e.target);
 })
---
+++ script2.js
@@ -0,0 +1,40 @@
+
+var apiKey="399885ef1dffc6fdfd42c5f1a6bdb34c";
+var appId="b2baeb67";
+var cuisineBtn=(".")
+// var cuisine=["Pasta", "Quick", "Soup", "Pizza", "Vegetable", "Sandwich"];
+
+function getRecipes(food) {
+    // $("#recipeDisplay").empty();
+    console.log("hungry");
+    var cuisine=$(food).attr("id");
+    var recipeURL="https://api.edamam.com/search?q=" + cuisine + "&app_id=" + appId + "&app_key=" + apiKey;
+    $.ajax ({
+        url: recipeURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        response.hits.forEach(element => {
+            var divStyled = $('<div>');
+            divStyled.attr('class','card');
+            var divFood = $('<div>');
+            var divMoreInfo = $('<button>');
+           divMoreInfo.html(`<a href="${element.recipe.url}">More Info</a>`)
+            divFood.text(element.recipe.label);
+            console.log(element);
+            divStyled.append(divFood).append(divMoreInfo);
+            $(".name").append(divStyled);
+            
+            // $(".recipe").append(element.recipe.url);
+            
+        });
+    })
+
+    }
+
+    $(document).on("click", ".food button", function (e) {
+        $(".name").empty();
+        $(".recipe").empty();
+        getRecipes($(e.target));
+    })
+
